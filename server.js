f108fd95ac4e96b2447d3a16d896a9383e8e4193
GeekAb <nit.abhi85@gmail.com>
2015-11-16 23:21:31 +0530

    Useful info file added

---
+++ server.js
@@ -0,0 +1,18 @@
+/*HTTP Object*/
+var http = require("http");
+
+/*Create server and bind it to port 8888*/
+//http.createServer(function(request, response) {
+//  response.writeHead(200, {"Content-Type": "text/plain"});
+//  response.write("Hello World");
+//  response.end();
+//}).listen(8888);
+
+/*Other way to print "Hello World"*/
+function onRequest(request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello World");
+  response.end();
+}
+
+http.createServer(onRequest).listen(8888);
